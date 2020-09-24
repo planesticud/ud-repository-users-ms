8121ab69e7ce1237b761c510b9f101423d1e0877
carloscaceres2093 <carlosacr2093@gmail.com>
2020-09-23 20:57:53 -0500

    test users ci

--- routers/routes.js
+++ routers/routes.js
@@ -19,5 +19,8 @@ router.delete(routers.USERS, wrap(usersController.deleteUsers))
 router.get(routers.HEALTH, wrap(async (req, res) => {
     res.status(OK).json({ message: 'OK' })
   }))
+  router.get("/test_users_ci", wrap(async (req, res) => {
+    res.status(OK).json({ message: 'THis a test for users ci' })
+  }))
 
 module.exports = router
